ff21653ca7b49c949aa861cdc64aa6b9fa22c4f0
fm247 <hiyoo95@gmail.com>
2021-08-27 02:17:52 +0900

    UPDATE: How to handle events in React.js (Call function or write function in attr

--- vocalist/src/component/Hello.js
+++ vocalist/src/component/Hello.js
@@ -3,9 +3,11 @@ export default function Hello() {
     const name = 'Alexander';
     console.log(name);
   };
-  const showAge = () => {
-    const age = 26;
+  const showAge = (age) => {
     console.log(age);
+  };
+  const showText = (txt) => {
+    console.log(txt);
   };
   return (
     <>
@@ -13,10 +15,27 @@ export default function Hello() {
       {/* 이름 콘솔 창에 출력 */}
       <button onClick={showName}>Show Name</button>
       {/* 나이 콘솔 창에 출력 */}
-      <button onClick={showAge}>Show Age</button>
+      <button
+        onClick={() => {
+          showAge(26);
+        }}
+      >
+        Show Age
+      </button>
+      <input
+        type="text"
+        onChange={(e) => {
+          const txt = e.target.value;
+          console.log(e.target.value);
+          console.log(txt);
+        }}
+      ></input>
     </>
   );
 }
 {
-  /* React에서 이벤트를 걸려면 미리 함수를 만들어 전달하는 방법이 있다. */
+  /* React에서 이벤트를 걸려면 
+  1.미리 함수를 만들어 전달하는 방법
+  2. onClick attribute 안에 함수를 직접 작성하는 법 => 장점: 매개변수를 전달하기 용이하다.
+  이 있다. */
 }
