cc4eb89572d2635e12a863932fc5bbf5a272a6a8
d_flex <hiyoo95@gmail.com>
2022-01-16 00:52:37 +0900

    tic tac toe game tutorial compleleted

--- tic-tac-to/src/index.js
+++ tic-tac-to/src/index.js
@@ -1,53 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 /* Board 컴포넌트에 따라 제어되는 컴포넌트 (Controlled) */
-class Square extends Component {
-  render() {
-    return (
-      <button className='square' onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
-}
+
+const Square = (props) => {
+  return (
+    <button className='square' onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
 
 /* Board의 State값이 변경될 때마다 
 자식인 Square는 자동으로 리렌더링 된다. */
+// const Board = () => {
+//   [squares, setSquares] = useState([])
+//   const handleClick = () => {
+//     setSquares(...)
+//   }
+//   return (
+
+//   );
+// }
+
 class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
-  handleClick = (i) => {
-    /* squares : {null*9} 로 초기화된 스테이트를 복사하는 코드 => 객체의 불변성 유지*/
-    const squares = this.state.squares.slice();
-    console.log(squares);
-
-    /* squares state의 각 idx 값에 'X'를 대입한다. */
-    squares[i] = 'X';
-    this.setState({ squares: squares });
-  };
   renderSquare(i) {
     return (
       <Square
         /* value prop 에 선언된 squares[i] 값을 전달한다. */
-        value={this.state.squares[i]}
+        value={this.props.squares[i]}
         /* handleClick의 인자로 i 인덱스 값을 넣어 Props로 전달한다 */
-        onClick={() => this.handleClick(i)}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className='status'>{status}</div>
         <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -69,20 +67,104 @@ class Board extends Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+    };
+  }
+  handleClick = (i) => {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    /* squares : {null*9} 로 초기화된 스테이트를 복사하는 코드 => 객체의 불변성 유지*/
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    /* squares state의 각 idx 값에 'X'를 대입한다. */
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares,
+        },
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  };
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? `Go to move ${move}` : `Go to game start`;
+
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
     return (
       <div className='game'>
         <div className='game-board'>
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className='game-info'>
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
   }
 }
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
 
 // ========================================
 
